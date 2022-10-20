2d9e398c1a4e2227475149f340263ebafd591de3
Francis_ldn <fnhh04@gmail.com>
2022-10-20 16:01:11 +0200

    edit title

--- src/scenes/Index/Navbar/index.tsx
+++ src/scenes/Index/Navbar/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 export function Navbar() {
   return (
     <header className={styles.container}>
-      <title className={styles.title}>Tasks Board</title>
+      <h1 className={styles.title}>Tasks Board</h1>
       <hr className={styles.horizontalLine} />
     </header>
   );
